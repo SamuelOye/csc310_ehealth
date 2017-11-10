72893846885ee44e0663a595dce715ed233d31dc
Samuel Oyewole <samueloye_gold@yahoo.com>
2017-11-10 08:31:09 +0100

    Removed gender field

--- profile.php
+++ profile.php
@@ -14,29 +14,29 @@
 	<?php include("navbar_in.php"); ?>
 	<div class="container">
 		<h3>Edit Profile</h3>
-			<form action="" method="post">
+			<form action="edit_profile.php" method="post">
 				<h5>Name:</h5>
 				<div class="row">
 					<div class=" input-field col s6">
-						<input id="firstname" name="first_name" type="text" class="validate">
+						<input id="firstname" name="first_name" type="text" class="validate" required>
 						<label for="firstname">Firstname</label>
 					</div>
 
 					<div class=" input-field col s6">
-						<input id="lastname" name="last_name" type="text" class="validate">
+						<input id="lastname" name="last_name" type="text" class="validate" required>
 						<label for="lastname">Lastname</label>
-					</div>	
+					</div>
 				</div>
 
 				<h5>Security:</h5>
 				<div class="row">
 					<div class="input-field col s6">
-					  <input id="password" name="password" type="password" class="validate">
+					  <input id="password" name="password" type="password" class="validate" required=>
 					  <label >Password</label>
 					</div>
 
 					<div class="input-field col s6">
-					  <input id="confirmpass" name="confirmpass" type="password" class="validate">
+					  <input id="confirmpass" name="confirmpass" type="password" class="validate" required>
 					  <label for="confirmpass">Confirm Password</label>
 					</div>
 				</div>
@@ -44,12 +44,12 @@
 				<h5>Mail: </h5>
 				<div class="row">
 				  <div class="input-field col s5" disabled>
-				    <input id="email" type="email" name="email" class="validate">
+				    <input id="email" type="email" name="email" class="validate" required>
 				    <label for="email">Email</label>
 				  </div>
 				</div>
 
-				 <h5>Gender:</h5>
+<!-- 				 <h5>Gender:</h5>
 		          <div class="row">
 		            <div class="col s4">
 		              <input class="with-gap" name="gender" type="radio" value="Male" checked>
@@ -61,9 +61,9 @@
 		              <label for="Female">Female</label>
 		            </div>
 		          </div>
-		          <div >
+		          <div > -->
 		          	<button class="btn waves-effect  teal lighten-2" type="submit"><i class="material-icons left">save</i>Save</button>
-		          	<button class="btn waves-effect " onclick=""><i class="material-icons left">cancel</i>Cancel</button>
+		          	<a class="btn waves-effect  " href="hompage.php"><i class="material-icons left">cancel</i>Cancel</a>
 		          </div>
 		          		
 			</form>	
